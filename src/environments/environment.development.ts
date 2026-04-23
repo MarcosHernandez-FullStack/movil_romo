cbbee3a61de249180d8e879848766708a7a2eb86
MarcosHernandez-FullStack <marcoshernandez171711@gmail.com>
2026-04-23 17:15:10 -0500

    Cambio de apiurl a desarrollo

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  //apiUrl: 'http://localhost:5016/api',
-  apiUrl: 'https://ca-back-romo.whitedesert-ca97fbc6.eastus2.azurecontainerapps.io/api',
+  apiUrl: 'http://localhost:5016/api',
+  //apiUrl: 'https://ca-back-romo.whitedesert-ca97fbc6.eastus2.azurecontainerapps.io/api',
   googleClientId: '498361177128-tgudn71ve78m73jlaqcn0dd4m5kbrehs.apps.googleusercontent.com',
 };
